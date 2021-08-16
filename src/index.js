99423f6c52e2fba7cb86ce1c6159bd7bdccc8c3a
Dzmitry Buraukin <dmitry.buravkin@gmail.com>
2021-08-16 22:07:47 +0300

    fixed: bugs with spaces

--- src/index.js
+++ src/index.js
@@ -41,12 +41,15 @@ module.exports = function toReadable (number) {
   if (number < 20) return ones[number];
 
   if (numStr.length === 2) {
-    return tens[+numStr[0]] + ' ' + ones[+numStr[1]];
+    if (numStr[1] == '0') {
+      return tens[+numStr[0]];
+    }
+    return tens[+numStr[0]] + ` ${ones[+numStr[1]]}`;
   }
 
   if (numStr.length === 3) {
     if (numStr[1] == '0' && numStr[2] == '0') {
-      return ones[+numStr[0]] + 'hundred';
+      return ones[+numStr[0]] + ' hundred';
     }
     return ones[+numStr[0]] + ' hundred ' + toReadable(+(numStr[1] + numStr[2]));
   }
