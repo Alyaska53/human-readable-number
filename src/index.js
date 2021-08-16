c8e9d46b464e19d0824b7638bec21df7d154d1c9
Dzmitry Buraukin <dmitry.buravkin@gmail.com>
2021-08-16 21:30:50 +0300

    feat: add toReadable() method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function toReadable (number) {
-  
+  const ones = [
+    '', 
+    'one', 
+    'two', 
+    'three', 
+    'four', 
+    'five', 
+    'six', 
+    'seven', 
+    'eight', 
+    'nine',
+    'ten', 
+    'eleven', 
+    'twelve', 
+    'thirteen', 
+    'fourteen', 
+    'fifteen', 
+    'sixteen',
+    'seventeen', 
+    'eighteen', 
+    'nineteen'
+  ];
+  const tens = [
+    '', 
+    '', 
+    'twenty', 
+    'thirty', 
+    'forty', 
+    'fifty', 
+    'sixty', 
+    'seventy', 
+    'eighty',
+    'ninety'
+  ];
+
+  let numStr = number.toString();
+
+  if (number === 0) return 'zero';
+
+  if (number < 20) return ones[number];
+
+  if (numStr.length === 2) {
+    return tens[+numStr[0]] + ' ' + ones[+numStr[1]];
+  }
+
+  if (numStr.length === 3) {
+    if (numStr[1] == '0' && numStr[2] == '0') {
+      return ones[+numStr[0]] + 'hundred';
+    }
+    return ones[+numStr[0]] + ' hundred ' + toReadable(+(numStr[1] + numStr[2]));
+  }
 }
